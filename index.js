cb7733b428fd162ee442142cee5680bdf9f52db9
Kashif1Naqvi <naqvishah582@gmail.com>
2019-11-21 09:52:03 +0500

    code updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const svg = d3.select("body").append("div").classed("svg-container", true).attr(
               .attr("viewBox", "0 0 600 400")
               .append("svg")
               .attr("class","svg-content-responsive")
-              .attr("width",2000)
+              .attr("width","100%")
               .attr("height",1500)
               // .style("background-color","black");
 function color(){
@@ -59,13 +59,13 @@ function colors(){
 
     let data =  d.map(d=>parseInt(d.close))
     let date =  d.map(d=>new Date(d.date))
+    date.sort((a,b)=> new Date(a.date) - new Date(b.date))    
 
     const db  = d3.pie().sort(null).value(d=>d)(data);
-
     const segment1 = d3.arc().innerRadius(21).outerRadius(315).padAngle(20).padRadius(20);
 
     let seg = svg.append("g").attr("transform","translate(999,700)").attr("class","node")
-     let graph =seg.append("g").attr("transform","translate(510,-250)").attr("class","pie")
+    let graph =  seg.append("g").attr("transform","translate(-444,290)").attr("class","pie")
                   graph.selectAll("path").data(db).enter().append("path")
                   .attr("d",segment1)
                   .attr("fill",(d,i)=>{
@@ -78,36 +78,42 @@ function colors(){
                   .attr("fill",function(d,i){
                     return colors(i)
                   })
+                  .transition()
+                  .duration(400)
+                  .delay(400);
 
                   let yScale = d3.scaleLinear().domain([0,d3.max(data)]).range([height,0])
                   let xScale = d3.scaleBand().domain(data).range([0,width]);
                   let yAxis = d3.axisLeft(yScale).ticks(5)
-                  let xValues = d3.scaleTime()
-                                  .domain([date[0],date[(date.length - 1)]])
-                                  .range([0,width])
-                  let xAxis   = d3.axisBottom(xValues).ticks(d3.timeDay.every(1))
+                  let xValues = d3.scaleTime().domain([date[0], date[date.length - 1 ]]).range([0,width])
+                  let xAxis = d3.axisBottom(xValues).ticks(25)
 
 let code = svg.append("g").attr("transform","translate(70,110)").attr("class","db")
                      code.selectAll("rect").attr("class","barchart").data(data).enter().append("rect")
                      .attr("height",function(d,i){
-                       return yScale(d);
+                      return yScale(d);
                      })
-                     .attr("width",function(d,i){
-                       return xScale.bandwidth();
-                     })
-                     .attr("x",function(d,i){
-                       return xScale(d)
-                     })
-                     .attr("y",function(d,i){
-                        return  height -  yScale(d)
-                     })
+                     .attr("width",10)
+                     .attr("x",302)
+                     .attr("y",-910)
                      .attr("fill",function(d,i){
                        return color(i)
+                      })
+                    .transition()
+                    .attr("y",function(d,i){
+                      return  height -  yScale(d)
                      })
-
+                   .attr("x",function(d,i){
+                    return xScale(d)
+                  })
+                  .attr("width",function(d,i){
+                    return xScale.bandwidth();
+                  })
+                  .duration((d,i)=>d*2)
+                  .delay((d,i)=>d*20)
                     d3.select(".svg-container svg").append("g").attr("transform","translate(61,110)")
-                                    .call(yAxis)
-                    d3.select(".svg-container svg").append("g").attr("transform",`translate(61,${height}+50)`)
+                      .call(yAxis)
+                    
+                    d3.select(".svg-container svg").append("g").attr("transform",`translate(60,619)`)
                     .call(xAxis)
-
-  })
+})
